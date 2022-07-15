50faa9e026c533a4250952d76ec51702f522f561
GB <GB@GBs-MacBook.local>
2022-07-15 16:40:55 -0400

    new tests

--- threeknights/threeknightsapp/tests.py
+++ threeknights/threeknightsapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from threeknightsapp.models import Exercise
 
 # Create your tests here.
+
+
+
+class PostTestCase(TestCase):
+    def testExercise(self):
+        exerc = Exercise(name="My Exercise1", points =3)
+        self.assertEqual(exerc.name, "My Exercise1")
+        self.assertEqual(exerc.points, 3)
+        
